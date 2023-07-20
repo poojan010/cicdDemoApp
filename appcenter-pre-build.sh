c5bb1ea3e03c4bc170cd4454b5c761a76ffbc0d2
Poojan Bhatt <pdbhatt010@gmail.com>
2023-07-20 10:23:24 +0530

    checking trigger type

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -10,39 +10,41 @@ echo "**************************************************************************
 echo "MY CUSTOM Pre-BUILD SCRIPT STARTS ..."
 echo "**************************************************************************************************"
 
+echo "APPCENTER_TRIGGER"
+echo $APPCENTER_TRIGGER
 
 
-echo "list files in APPCENTER_SOURCE_DIRECTORY"
-ls $APPCENTER_SOURCE_DIRECTORY
+# echo "list files in APPCENTER_SOURCE_DIRECTORY"
+# ls $APPCENTER_SOURCE_DIRECTORY
 
-# Run Android APPDebug & APPTest
-cd $APPCENTER_SOURCE_DIRECTORY/android
-# ./gradlew assembleDebug
-./gradlew assembleRelease
-./gradlew assembleAndroidTest
+# # Run Android APPDebug & APPTest
+# cd $APPCENTER_SOURCE_DIRECTORY/android
+# # ./gradlew assembleDebug
+# ./gradlew assembleRelease
+# ./gradlew assembleAndroidTest
 
-# variables
-appCenterLoginApiToken=$APPCENTER_ACCESS_TOKEN
-locale="en_US"
-appName="CI-CD-POC/cicdDemoApp-1"
-deviceSetName="CI-CD-POC/android-devices"
-testSeriesName="test-series"
-appDebugPath=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/debug/app-debug.apk
-appReleasePath=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk
-buildDir=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/androidTest/debug
+# # variables
+# appCenterLoginApiToken=$APPCENTER_ACCESS_TOKEN
+# locale="en_US"
+# appName="CI-CD-POC/cicdDemoApp-1"
+# deviceSetName="CI-CD-POC/android-devices"
+# testSeriesName="test-series"
+# appDebugPath=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/debug/app-debug.apk
+# appReleasePath=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/release/app-release.apk
+# buildDir=$APPCENTER_SOURCE_DIRECTORY/android/app/build/outputs/apk/androidTest/debug
 
-# Run UITest if branch is master
-if [ "$APPCENTER_BRANCH" == "dev" ];
-then
-    # app center command espresso test
-    echo "########## $appName espresso start ##########"
-    appcenter test run espresso --app $appName --devices $deviceSetName --app-path $appReleasePath --test-series $testSeriesName --locale $locale --build-dir $buildDir --token $appCenterLoginApiToken;
-    echo "########## $appName espresso finished ##########"
-else
-    echo "Current branch is not 'dev'"
-fi
+# # Run UITest if branch is master
+# if [ "$APPCENTER_BRANCH" == "dev" ];
+# then
+#     # app center command espresso test
+#     echo "########## $appName espresso start ##########"
+#     appcenter test run espresso --app $appName --devices $deviceSetName --app-path $appReleasePath --test-series $testSeriesName --locale $locale --build-dir $buildDir --token $appCenterLoginApiToken;
+#     echo "########## $appName espresso finished ##########"
+# else
+#     echo "Current branch is not 'dev'"
+# fi
 
-cd ..
+# cd ..
 
 echo 
 echo "**************************************************************************************************"
