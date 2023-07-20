ab6a2d68cf5a92bbf5d5f8f06a4385d43bf8950d
Poojan Bhatt <pdbhatt010@gmail.com>
2023-07-20 08:58:41 +0530

    updated scripts

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -12,30 +12,30 @@ echo "**************************************************************************
 
 
 # Run Android APPDebug & APPTest
-$APPCENTER_SOURCE_DIRECTORY/gradlew assembleDebug
-$APPCENTER_SOURCE_DIRECTORY/gradlew assembleAndroidTest
-# $APPCENTER_SOURCE_DIRECTORY/gradlew assembleRelease
+# $APPCENTER_SOURCE_DIRECTORY/gradlew assembleDebug
+# $APPCENTER_SOURCE_DIRECTORY/gradlew assembleAndroidTest
+# # $APPCENTER_SOURCE_DIRECTORY/gradlew assembleRelease
 
-# variables
-appCenterLoginApiToken=$APPCENTER_ACCESS_TOKEN
-locale="en_US"
-appName="CI-CD-POC/cicdDemoApp-1"
-deviceSetName="CI-CD-POC/android-devices"
-testSeriesName="test-series"
-appDebugPath=$APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/debug/app-debug.apk
-appReleasePath=$APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/release/app-release.apk
-buildDir=$APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/androidTest/debug
+# # variables
+# appCenterLoginApiToken=$APPCENTER_ACCESS_TOKEN
+# locale="en_US"
+# appName="CI-CD-POC/cicdDemoApp-1"
+# deviceSetName="CI-CD-POC/android-devices"
+# testSeriesName="test-series"
+# appDebugPath=$APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/debug/app-debug.apk
+# appReleasePath=$APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/release/app-release.apk
+# buildDir=$APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/androidTest/debug
 
-# Run UITest if branch is master
-if [ "$APPCENTER_BRANCH" == "dev" ];
-then
-    # app center command espresso test
-    echo "########## $appName espresso start ##########"
-    appcenter test run espresso --app $appName --devices $deviceSetName --app-path $appDebugPath --test-series $testSeriesName --locale $locale --build-dir $buildDir --token $appCenterLoginApiToken;
-    echo "########## $appName espresso finished ##########"
-else
-    echo "Current branch is not 'dev'"
-fi
+# # Run UITest if branch is master
+# if [ "$APPCENTER_BRANCH" == "dev" ];
+# then
+#     # app center command espresso test
+#     echo "########## $appName espresso start ##########"
+#     appcenter test run espresso --app $appName --devices $deviceSetName --app-path $appDebugPath --test-series $testSeriesName --locale $locale --build-dir $buildDir --token $appCenterLoginApiToken;
+#     echo "########## $appName espresso finished ##########"
+# else
+#     echo "Current branch is not 'dev'"
+# fi
 
 
 
